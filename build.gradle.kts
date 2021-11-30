cfce3df103419cc2b41ced4007a9abcd35ad6fe4
hYdos <haydenv06@gmail.com>
2021-11-30 16:42:27 +1000

    Gradle Wrapper

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+}
+
+group = "net.legacyfabric"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.cadixdev", "lorenz", "0.5.8")
+}
